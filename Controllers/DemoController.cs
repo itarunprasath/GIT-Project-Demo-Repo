fad7c06e1d5fc9128067d6258b45f87b9a9fa3fc
arunt <itarunprasath@gmail.com>
2024-12-12 09:59:39 +0800

    added demo controller and added space in program

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GIT_Project_Demo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult WelcomeMessage()
+        {
+            return Ok("welcome to demo");
+        }
+    }
+}
